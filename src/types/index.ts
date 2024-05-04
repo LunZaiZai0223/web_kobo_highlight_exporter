e7793872e23c23b4080b5df84fa93add44e896db
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2024-05-04 21:25:38 +0800

    feat: add basic types

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export type Book = {
+  title: string
+  contentId: string
+  author: string
+  publisher: string
+  isbn: string
+  releaseDate: string
+  readPercent: string
+  lastRead: string
+}
+
+export type BookHighlight = {
+  highlightText: string
+  annotation: string
+}
