24266e7ff65c2389cdbf1f9c5cc7b9b0826e3d9a
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2024-05-04 21:55:51 +0800

    style: modify style

--- src/components/MainContainer/index.tsx
+++ src/components/MainContainer/index.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react'
 const MainContainer = (props: PropsWithChildren) => {
   const { children } = props
 
-  return <main className={'max-w-[1400px] px-8'}>{children}</main>
+  return <main className={'max-w-[1400px] px-8 mt-0 mx-auto mb-6'}>{children}</main>
 }
 
 export default MainContainer
