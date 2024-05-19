c015457a3a396fb7839760df30277a2efce13fff
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2024-05-20 00:10:33 +0800

    feat: create constants

---
+++ src/constants/shared.ts
@@ -0,0 +1,7 @@
+/**
+ * 這裡是不是最後要搞一個 server 去 host 這個東西？
+ * 先暫時寫其他專案的線上網址 ref. https://github.com/inloop/sqlite-viewer/blob/gh-pages/js/main.js#L103
+ */
+const SQL_WASM_PATH = 'https://inloop.github.io/sqlite-viewer/js/sql-wasm.wasm'
+
+export { SQL_WASM_PATH }
